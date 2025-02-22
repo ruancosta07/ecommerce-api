300de218f128067ffe3a0a54fbdc38c51b3fb8f3
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-21 23:41:05 -0300

    app module

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { OrdersModule } from './orders/orders.module';
+import { StripeService } from './utils/stripe';
+import { ConfigModule, } from '@nestjs/config';
+import { PrismaService } from './services/prisma';
+import { WebhookService } from './webhook/webhook.service';
+import { WebhookController } from './webhook/webhook.controller';
+import { WebhookModule } from './webhook/webhook.module';
+import { NodemailerService } from './utils/nodemailer';
+@Module({
+  imports: [ UsersModule, AuthModule, ProductsModule, ReviewsModule, OrdersModule, ConfigModule.forRoot({isGlobal:true, envFilePath: ".env"}), WebhookModule],
+  providers: [PrismaService,StripeService, WebhookService, NodemailerService],
+  exports: [StripeService, NodemailerService],
+  controllers: [WebhookController],
+})
+export class AppModule {}
