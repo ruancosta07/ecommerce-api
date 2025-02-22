dd8022c2d6edd388626cab86812b2dca439387e6
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-21 23:38:36 -0300

    controller webhook

---
+++ src/webhook/webhook.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Headers, Post, RawBodyRequest, Req } from '@nestjs/common';
+import { WebhookService } from './webhook.service';
+import { StripeService } from 'src/utils/stripe';
+import { Request } from 'express';
+
+@Controller('webhook')
+export class WebhookController {
+    constructor (private readonly webhookService:WebhookService, private readonly stripe:StripeService){
+
+    }
+    @Post()
+    async handleWebhook(@Req( ) req:RawBodyRequest<Request>, @Headers("stripe-signature") signature:string){
+        return this.webhookService.handleWebhook(req, signature)
+    }
+}
