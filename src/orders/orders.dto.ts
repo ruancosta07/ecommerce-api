70ad01fdcdd13d4660bd76ef8c32fd6af34c84bd
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-21 23:35:56 -0300

    orders dto

---
+++ src/orders/orders.dto.ts
@@ -0,0 +1,14 @@
+export class OrderDto {
+    id: string;
+    userId: string;
+    status: 'pending' | 'approved' | 'expired' | "canceled";
+    productId: string;
+    estimatedDeliveryDate: Date;
+    price:number
+    expireDate:Date
+    stripePaymentId?:string
+    stripeSessionId:string
+    checkoutUrl:string
+    checkoutUrlExpiresAt:number
+  }
+  
