5897f445f8481db613548a5a7f77329f0c812c2d
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-21 23:35:56 -0300

    orders module

---
+++ src/orders/orders.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { OrdersController } from './orders.controller';
+import { PrismaService } from '../services/prisma';
+import { StripeService } from 'src/utils/stripe';
+
+@Module({
+  controllers: [OrdersController],
+  providers: [OrdersService, PrismaService, StripeService],
+  
+})
+export class OrdersModule {}
