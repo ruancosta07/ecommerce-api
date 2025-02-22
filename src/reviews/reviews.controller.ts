acdce1c6f9b87fae36bb24615ce128a02b29d94b
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-21 23:37:19 -0300

    controller reviews

---
+++ src/reviews/reviews.controller.ts
@@ -0,0 +1,29 @@
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { ReviewsService } from './reviews.service';
+import { Request } from 'express';
+import { ReviewDto } from './reviews.dto';
+
+@Controller('reviews')
+export class ReviewsController {
+  constructor(private readonly reviewsService: ReviewsService) { }
+
+  @Get()
+  getAll(){
+    return this.reviewsService.getAllReviews()
+  }
+
+  @Get(":id")
+  getReviews(@Param("id") id: string):Promise<ReviewDto[]> {
+    return this.reviewsService.getReviews(id)
+  }
+  @Get("review/:id")
+  getSingleReview(@Param("id") id: string):Promise<ReviewDto> {
+    return this.reviewsService.getSingleReview(id)
+  }
+
+  @Post("create/:id")
+  createReview(@Req() req: Request, @Body() review: ReviewDto, @Param("id") id: string) {
+    return this.reviewsService.addReview(req, review, id)
+  }
+
+}
