057be459f035557a84be19180ddaa47650c5dc24
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-02-21 23:36:39 -0300

    products dto

---
+++ src/products/products.dto.ts
@@ -0,0 +1,17 @@
+import { ReviewDto } from "src/reviews/reviews.dto";
+
+export class ProductsDto {
+    id: string;
+    name: string;
+    price: number;
+    description: string;
+    images?: string[];
+    tags: string[];
+    order: string[];
+    userId: string;
+    reviews:ReviewDto[]
+    stripeProductId?:string
+    constructor(partial: Partial<ProductsDto>) {
+        Object.assign(this, partial);
+    }
+}
