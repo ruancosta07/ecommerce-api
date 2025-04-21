e0a8f4120d5e2eab4642fcee56fb9430d9dcdc13
ruancosta07 <ruancosta.ti0805@gmail.com>
2025-04-21 14:34:42 -0300

    feat: rotas de login do github e google

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,21 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiBody } from '@nestjs/swagger';
 import { UserDto } from '../users/users.dto';
 import { AuthDto } from './auth.dto';
-import { Request } from 'express';
+import { Request, Response } from 'express';
+import { AuthGuard } from '@nestjs/passport';
+
+interface GoogleRequest extends Omit<Request, "user"> {
+  user: {
+    id:string;
+    email:string;
+    provider:string;
+    name:string;
+    avatar:string;
+    jwtToken:string
+  }
+}
 
 @Controller('auth')
 export class AuthController {
@@ -22,5 +34,29 @@ export class AuthController {
   @Post("verify")
   verify(@Req() req:Request){
     return this.authService.verifyUser(req)
+  }
+  @Get("google")
+  @UseGuards(AuthGuard("google"))
+  async authGoogle(){
+
+  }
+  @Get("google/redirect")
+  @UseGuards(AuthGuard("google"))
+  async googleRedirect(@Req()req:GoogleRequest, @Res() res:Response){
+    const {jwtToken} = req.user 
+    return res.redirect(`http://localhost:5173/login?token=${jwtToken}`)
+  }
+
+  @Get("github")
+  @UseGuards(AuthGuard("github"))
+  async authGithub(){
+
+  }
+
+  @Get("github/redirect")
+  @UseGuards(AuthGuard("github"))
+  async githubRedirect(@Req()req:GoogleRequest, @Res() res:Response){
+    const {jwtToken} = req.user 
+    return res.redirect(`http://localhost:5173/login?token=${jwtToken}`)
   }
 }
